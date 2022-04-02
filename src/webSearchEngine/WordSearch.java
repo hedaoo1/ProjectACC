f43618f272ae959007c4e7b6968a9d2b133c1d8c
hedaoo1 <98249851+hedaoo1@users.noreply.github.com>
2022-04-02 09:32:07 -0400

    Update WordSearch.java

--- src/webSearchEngine/WordSearch.java
+++ src/webSearchEngine/WordSearch.java
@@ -2,26 +2,15 @@ package webSearchEngine;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class WordSearch {
 
 	static Scanner sc = new Scanner(System.in);
 
-	// get occurence and position of word
 	public static int searchGivenWord(File filePath, String s1) throws IOException {
 		int counter = 0;
 		String data = "";
@@ -40,15 +29,12 @@ public class WordSearch {
 		}
 		String txt = data;
 		processing.BoyerMoore offset1 = new processing.BoyerMoore(s1);
-
 		int offset = 0;
-
 		for (int loc = 0; loc <= txt.length(); loc += offset + s1.length()) {
-
 			offset = offset1.search(s1, txt.substring(loc));
 			if ((offset + loc) < txt.length()) {
 				counter++;
-				System.out.println(s1 + " is at position " + (offset + loc)); // printing position of word
+				System.out.println(s1 + " is at position " + (offset + loc));
 			}
 		}
 		if (counter != 0) {
@@ -59,44 +45,38 @@ public class WordSearch {
 		return counter;
 	}
 
-	public static void websearch() {
+	public static void search() {
 
 		Hashtable<String, Integer> htable = new Hashtable<String, Integer>();
 		Scanner s = new Scanner(System.in);
-
 		System.out.println("Enter Y/y to search and N/n to exit : ");
 		String choice = s.nextLine();
-		String p;
+		String word;
 
 		while (choice.equals("Y") | choice.equals("y")) {
-			System.out.println("Enter your search : ");
-			p = s.nextLine();
-
+			System.out.println("Enter the word to search : ");
+			word = s.nextLine();
 			long fileNumber = 0;
-			int frequency = 0;
-			int rep = 0; // No. of files that contains the Searched word
+			int wordFrequency = 0;
+			int wordInFiles = 0; 
 			try {
-				// long startTime = System.currentTimeMillis();
-				long startTimesearch = System.currentTimeMillis();
-				File dir = new File("Resources/Text");
-
-				File[] fileArray = dir.listFiles();
-				for (int i = 0; i < fileArray.length; i++)
-
+				long startSearchTime = System.currentTimeMillis();
+				File directory = new File("Resources/Text");
+				File[] fileList = directory.listFiles();
+				for (int i = 0; i < fileList.length; i++)
 				{
-					frequency = searchGivenWord(fileArray[i], p);
-					htable.put(fileArray[i].getName(), frequency);
-					if (frequency != 0) {
-						rep++;
-						System.out.println("\nFile containing " + p + " word is= " + fileArray[i]);
+					wordFrequency = searchGivenWord(fileList[i], word);
+					htable.put(fileList[i].getName(), wordFrequency);
+					if (wordFrequency != 0) {
+						wordInFiles++;
+						System.out.println("\nFile containing " + word + " word is= " + fileList[i]);
 					}
 					fileNumber++;
 				}
-
-				System.out.println("\nNumber of time the word " + p + " repeated is= " + rep);
-				long endTimesearch = System.currentTimeMillis();
+				System.out.println("\nNumber of time the word " + word + " repeated is= " + wordInFiles);
+				long endSearchTime = System.currentTimeMillis();
 				System.out.println(
-						"\nThe search took  " + (endTimesearch - startTimesearch) + " Milli Seconds to complete");
+						"\nThe search took  " + (endSearchTime - startSearchTime) + " Milli Seconds to complete");
 				System.out.println("Enter Y/y to search and N/n to exit : ");
 				choice = s.nextLine();
 
@@ -105,5 +85,4 @@ public class WordSearch {
 			}
 		}
 	}
-
 }
